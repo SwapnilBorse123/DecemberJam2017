c93fbc9675e95105f9a60bf4286edccf563f7da4
SwapnilBorse0307 <swpnl.borse@gmail.com>
2018-01-14 23:45:02 -0500

    Leetcode: finding max depth of BST

---
+++ lcMaxDepthBST.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int maximum(int a, int b){
+        return a > b ? a : b;
+    }
+    
+    int maxDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        
+        return (1 + max(maxDepth(root->left), maxDepth(root->right)));
+    }
+};
